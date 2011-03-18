3ed19e76188288a45daee70c8d21450faefdaac5
Thomas Kappler <tkappler@googlemail.com>
2011-03-18 20:18:26 +0100

    Various work on the site.

--- rebuild.sh
+++ rebuild.sh
@@ -3,4 +3,5 @@
 rm -rf out/*
 cp -R static out/
 cp CNAME out/
+perl store_categories.pl
 perl blosxom/blosxom.cgi -password='pass'
--- store_categories.pl
+++ store_categories.pl
@@ -9,29 +9,61 @@ use Storable;
 
 
 my $CATEGORIES = 'categories';
-my $STORAGE = '.categories';
+my $BLOSXOM_FILE_EXTENSION = 'text';
+
+my $ARTICLES_PER_CATEGORY_STORAGE = '.categories';
+my $META_PER_FILE_STORAGE = '.meta';
 
 my $writing_dir = $ARGV[0];
 die "Please give the directory containing your writing" unless $writing_dir;
 
-my %files_per_tag = ();
+my %articles_per_category = ();
+my %meta_per_file = ();
 
 sub handle_file {
-    say $_;
-    open (my $fh, '<', $_) or die "$_: $!";
+    my $file = $_;
+
+    return unless $file =~ /\.$BLOSXOM_FILE_EXTENSION$/;
+    # Strip the ending.
+    (my $article = $_) =~ s/\.$BLOSXOM_FILE_EXTENSION//;
+    say $article;
+
+    open (my $fh, '<', $file) or die "$file: $!";
+
+    # Record the title in the first line. For consistency it should be
+    # a meta value, too, but I couldn't get it to work in the
+    # templates.
+    my $first_line = 1;
+    my $title;
+
     while (my $line = <$fh>) {
+
+        # Title; see above.
+        if ($first_line) {
+            $title = $line;
+            $meta_per_file{title} = $title;
+            $first_line = 0;
+            next;
+        }
+
+        # Meta tag of the form "key: value"?
         my ($key, $value) = ($line =~ m!^(.+?)\s*:\s*(.+)$!);  # from meta
         next unless (defined $key and $key eq $CATEGORIES);
 
+        my @categories = ();
         for my $category (split /, */, $value) {
-            if (not exists $files_per_tag{$category}) {
-                $files_per_tag{$category} = [];
+            push @categories, $category;
+
+            if (not exists $articles_per_category{$category}) {
+                $articles_per_category{$category} = [];
             }
-            push @{$files_per_tag{$category}}, $_;
+            push @{$articles_per_category{$category}}, $article;
         }
+        $meta_per_file{$article} = \@categories;
     }
 }
 
 find(\&handle_file, ($writing_dir));
 
-store \%files_per_tag, $STORAGE;
+store \%articles_per_category, $ARTICLES_PER_CATEGORY_STORAGE;
+# store \%meta_per_file, $META_PER_FILE_STORAGE
