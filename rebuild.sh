72f2bf3e9a1661f15e4b3e500dc415598c88b64e
Thomas Kappler <tkappler@googlemail.com>
2011-03-19 15:17:25 +0100

    Bugfix: forgot script argument.

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+writing_dir=writing
+
 rm -rf out/*
 cp -R static out/
 cp CNAME out/
-perl store_categories.pl
+perl store_categories.pl $writing_dir
 perl blosxom/blosxom.cgi -password='pass'
